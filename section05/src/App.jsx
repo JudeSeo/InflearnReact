b17591237755ab12b2a35385afa536314a8deb04
JudeSeo <gn0408@gmail.com>
2024-03-22 19:44:26 +0900

    section05.4 완료

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,15 +1,22 @@
 import './App.css'
-import Header from "./Components/Header";
-import Main from "./Components/Main.jsx";
-import Footer from "./Components/Footer.jsx";
+import Button from "./Components/Button.jsx";
+import Header from "./Components/Header.jsx";
+
+const buttonProps = {
+    text: "메일",
+    color: "red",
+    a: 1,
+    b: 2,
+    c: 3,
+}
 
 // 부모 컴포넌트
 function App() {
     return (
         <>
-            <Header/>
-            <Main/>
-            <Footer/>
+            <Button {...buttonProps}/>
+            <Button text={"카페"}/>
+            <Button text={"블로그"}><Header /></Button>
         </>
     )
 }
