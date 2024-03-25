5d300f09c4232cd144224d9c262d518ef9cc5f1c
JudeSeo <gn0408@gmail.com>
2024-03-25 20:19:48 +0900

    section05.7 완료

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,21 +1,14 @@
 import './App.css'
-import {useState} from "react";
+import Bulb from "./Components/Bulb.jsx";
+import Counter from "./Components/Counter.jsx";
 
 function App() {
-    const [count, setCount] = useState(0);
-    const [light, setLight] = useState("OFF");
-    return (<>
-            <div>
-                <h1>{light}</h1>
-                <button onClick={() => setLight(light === "ON" ? "OFF" : "ON")}>
-                    {light === "ON" ? "끄기" : "켜기"}
-                </button>
-            </div>
-            <div>
-                <h1>{count}</h1>
-                <button onClick={() => setCount(count + 1)}>+</button>
-            </div>
-        </>)
+    return (
+        <>
+            <Bulb />
+            <Counter />
+        </>
+    )
 }
 
 export default App
