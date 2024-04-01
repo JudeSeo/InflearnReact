9b7addc9f2192bf03d90a62c20d2255405ca73b6
JudeSeo <gn0408@gmail.com>
2024-04-01 18:18:05 +0900

    section07.3 완료

--- section07/src/App.jsx
+++ section07/src/App.jsx
@@ -1,18 +1,30 @@
 import './App.css'
 import Viewer from "./component/Viewer.jsx";
 import Controller from "./component/Controller.jsx";
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
+import Even from "./component/Even.jsx";
 
 function App() {
     const [count, setCount] = useState(0);
     const [input, setInput] = useState("");
 
+    const isMount = useRef(false);
+
+    // 1. 마운트: 탄생
     useEffect(() => {
-        console.log(`count: ${count} / input: ${input}`);
-    }, [count, input]);
-    // 의존성 배열
-    // dependency array
-    // deps
+        console.log("mount");
+    }, []);
+
+    // 2. 업데이트: 변화, 리렌더링
+    useEffect(() => {
+        if (!isMount.current) {
+            isMount.current = true;
+            return;
+        }
+        console.log("update");
+    });
+
+    // 3. 언마운트: 죽음
 
     const onClickButton = (value) => {
         setCount(count + value);
@@ -22,10 +34,11 @@ function App() {
         <div className={"App"}>
             <h1>Simple Counter</h1>
             <section>
-                <input value={input} onChange={(e) => setInput(e.target.value)} />
+                <input value={input} onChange={(e) => setInput(e.target.value)}/>
             </section>
             <section>
                 <Viewer count={count}/>
+                {count % 2 === 0 ? <Even /> : null}
             </section>
             <section>
                 <Controller onClickButton={onClickButton}/>
