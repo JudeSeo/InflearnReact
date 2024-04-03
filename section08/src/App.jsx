480ff74db177f401012f3b66a5672358b151c0fd
JudeSeo <gn0408@gmail.com>
2024-04-03 20:41:48 +0900

    section08.3 완료

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -2,8 +2,30 @@ import './App.css'
 import Header from "./component/Header.jsx";
 import Editor from "./component/Editor.jsx";
 import List from "./component/List.jsx";
+import {useState} from "react";
 
+const mockData = [
+    {
+        id: 1,
+        isDone: false,
+        content: "React 공부하기",
+        date: new Date().getTime(),
+    },
+    {
+        id: 2,
+        isDone: false,
+        content: "빨래하기",
+        date: new Date().getTime(),
+    },
+    {
+        id: 3,
+        isDone: false,
+        content: "노래하기",
+        date: new Date().getTime(),
+    }
+]
 function App() {
+    const [todos, setTodos] = useState(mockData)
     return (
         <div className={"App"}>
             <Header/>
