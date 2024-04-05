c34505c4ef19c8d63ebf8221716593c29eef1576
JudeSeo <gn0408@gmail.com>
2024-04-05 18:31:03 +0900

    section09.1 완료

---
+++ section09/src/component/TodoItem.jsx
@@ -0,0 +1,21 @@
+import "./TodoItem.css"
+
+const TodoItem = ({id, isDone, content, date, onUpdate, onDelete}) => {
+    const onChangeCheckbox = (e) => {
+        onUpdate(id);
+    }
+
+    const onClickDeleteButton = () => {
+        onDelete(id)
+    }
+
+    return (
+        <div className={"TodoItem"}>
+            <input readOnly={true} checked={isDone} type={"checkbox"} onChange={onChangeCheckbox}/>
+            <div className={"content"}>{content}</div>
+            <div className={"date"}>{new Date(date).toLocaleDateString()}</div>
+            <button onClick={onClickDeleteButton}>삭제</button>
+        </div>
+    )
+}
+export default TodoItem;
