49e59074e35ef4a7c07ab60da77233417e676ec8
JudeSeo <gn0408@gmail.com>
2024-04-15 23:00:59 +0900

    section12.4 완료

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,4 +1,4 @@
-import {Route, Routes} from "react-router-dom";
+import {Link, Route, Routes, useNavigate} from "react-router-dom";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Notfound from "./pages/Notfound";
@@ -9,13 +9,26 @@ import Home from "./pages/Home.jsx";
 // 3. "/diary": 일기를 상세히 조회하는 Diary 페이지
 
 function App() {
+    const nav = useNavigate();
+
+    const onClickButton = () => {
+        nav('/new')
+    }
     return (
-        <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/new" element={<New/>}/>
-            <Route path="/diary" element={<Diary/>}/>
-            <Route path="*" element={<Notfound/>}/>
-        </Routes>
+        <>
+            <div>
+                <Link to={"/"}>Home</Link>
+                <Link to={"/new"}>New</Link>
+                <Link to={"/diary"}>Diary</Link>
+            </div>
+            <button onClick={onClickButton}>New 페이지로 이동</button>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/new" element={<New/>}/>
+                <Route path="/diary" element={<Diary/>}/>
+                <Route path="*" element={<Notfound/>}/>
+            </Routes>
+        </>
     )
 }
 
