598612d5a33be86bce57cc06f089c071d65c51a2
JudeSeo <gn0408@gmail.com>
2024-04-17 18:45:13 +0900

    section12.8 완료

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,40 +1,43 @@
-import {Route, Routes, useNavigate} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Notfound from "./pages/Notfound";
 import Home from "./pages/Home.jsx";
-import Button from "./components/Button.jsx";
-import Header from "./components/Header.jsx";
+import Edit from "./pages/Edit.jsx";
+import {useReducer} from "react";
 
 // 1. "/": 모든 일기를 조회하는 Home 페이지
 // 2. "/new": 새로운 일기를 작성하는 New 페이지
 // 3. "/diary": 일기를 상세히 조회하는 Diary 페이지
 
-function App() {
-    const nav = useNavigate();
+const mockData = [
+    {
+        id: 1,
+        createData: new Date().getTime(),
+        emotionId: 1,
+        content: "1번 일기 내용"
+    },
+    {
+        id: 2,
+        createData: new Date().getTime(),
+        emotionId: 2,
+        content: "2번 일기 내용"
+    },
+]
 
-    const onClickButton = () => {
-        nav('/new')
-    }
+function reducer(state, action) {
+    return state;
+}
+
+function App() {
+    const [data, dispatch] = useReducer(reducer, mockData);
     return (
         <>
-            <Header title={"Header"}
-                    leftChild={<Button text={"Left"}/>}
-                    rightChild={<Button text={"Right"}/>}
-            />
-            <Button text={"123"} type={"POSITIVE"} onClick={() => {
-                console.log("123버튼")
-            }}/>
-            <Button text={"123"} type={"POSITIVE"} onClick={() => {
-                console.log("123버튼")
-            }}/>
-            <Button text={"123"} type={"NEGATIVE"} onClick={() => {
-                console.log("123버튼")
-            }}/>
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/new" element={<New/>}/>
                 <Route path="/diary/:id" element={<Diary/>}/>
+                <Route path="/edit/:id" element={<Edit/>}/>
                 <Route path="*" element={<Notfound/>}/>
             </Routes>
         </>
