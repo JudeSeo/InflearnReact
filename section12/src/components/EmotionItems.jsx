cbcfab31a034ba75cc1a7128569960cb32272af5
JudeSeo <gn0408@gmail.com>
2024-04-23 18:35:27 +0900

    section12.14 완료

--- section12/src/components/EmotionItems.jsx
+++ section12/src/components/EmotionItems.jsx
@@ -1,8 +1,8 @@
 import "./EmotionItems.css"
 import getEmotionImage from "../util/get-emotion-image.js";
 
-const EmotionItems = ({emotionId, emotionName, isSelected}) => {
-    return <div className={`EmotionItems ${isSelected ? `EmotionItem_on_${emotionId}` : ""}`}>
+const EmotionItems = ({emotionId, emotionName, isSelected, onClick}) => {
+    return <div onClick={onClick} className={`EmotionItems ${isSelected ? `EmotionItem_on_${emotionId}` : ""}`}>
         <img className={"emotion_img"} src={getEmotionImage(emotionId)}/>
         <div className={"emotion_name"}>{emotionName}</div>
     </div>
