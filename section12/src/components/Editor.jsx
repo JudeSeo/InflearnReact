38c5f9c82d43445d5227110684a177d9fbe52d20
JudeSeo <gn0408@gmail.com>
2024-04-25 18:21:18 +0900

    section12.16 완료

--- section12/src/components/Editor.jsx
+++ section12/src/components/Editor.jsx
@@ -3,29 +3,9 @@ import EmotionItems from "./EmotionItems.jsx";
 import Button from "./Button.jsx";
 import {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
+import {emotionList} from "../util/constants.js";
+import {getStringedDate} from "../util/getStringedDate.js";
 
-const emotionList = [
-    {
-        emotionId: 1,
-        emotionName: "완전 좋음"
-    },
-    {
-        emotionId: 2,
-        emotionName: "좋음"
-    },
-    {
-        emotionId: 3,
-        emotionName: "그럭저럭"
-    },
-    {
-        emotionId: 4,
-        emotionName: "나쁨"
-    },
-    {
-        emotionId: 5,
-        emotionName: "끔찍함"
-    },
-]
 const Editor = ({initData, onSubmit}) => {
     const [input, setInput] = useState({
         createdDate: new Date(),
@@ -44,20 +24,7 @@ const Editor = ({initData, onSubmit}) => {
 
     const nav = useNavigate();
     const emotionId = 1;
-    const getStringedDate = (targetDate) => {
-        // 날짜 => YYYY-MM-DD
-        let year = targetDate.getFullYear();
-        let month = targetDate.getMonth() + 1;
-        let date = targetDate.getDate();
 
-        if (month < 10) {
-            month = `0${month}`;
-        }
-        if (date < 10) {
-            date = `0${date};`
-        }
-        return `${year}-${month}-${date}`
-    }
 
     const onChangeInput = (e) => {
         let name = e.target.name;
